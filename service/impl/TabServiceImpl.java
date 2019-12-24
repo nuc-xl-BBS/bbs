c766f788057ac319c8e6eba07380f7c58d761ac5
2333chan <59174100+2333chan@users.noreply.github.com>
2019-12-24 14:57:39 +0800

    Add files via upload

---
+++ service/impl/TabServiceImpl.java
@@ -0,0 +1,36 @@
+package com.withstars.service.impl;
+
+import com.withstars.dao.TabMapper;
+import com.withstars.domain.Tab;
+import com.withstars.service.TabService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TabServiceImpl implements TabService {
+
+    @Autowired
+    public TabMapper tabDao;
+
+    public List<Tab> getAllTabs() {
+        return tabDao.getAllTabs();
+    }
+
+    public Tab getByTabNameEn(String tabNameEn) {
+        return tabDao.getByTabNameEn(tabNameEn);
+    }
+
+    public int insert(Tab tab) {
+        return tabDao.insert(tab);
+    }
+
+    public int deleteByPrimaryKey(Integer integer) {
+        return tabDao.deleteByPrimaryKey(integer);
+    }
+
+    public int updateByPrimaryKeySelective(Tab tab) {
+        return tabDao.updateByPrimaryKeySelective(tab);
+    }
+}
